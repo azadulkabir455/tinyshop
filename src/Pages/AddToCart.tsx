69611cf3757a639ae8f0818dc91da1aa7173b7a7
Azad ul kabir <azadulkabir10@gmail.com>
2022-11-10 14:30:49 +0600

    Tiny shop add to cart page done. All functionality done for shoping

--- src/Pages/AddToCart.tsx
+++ src/Pages/AddToCart.tsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import { useSelector, useDispatch } from "react-redux"
+import { BsDash, BsPlus, BsXCircleFill } from "react-icons/bs";
 
 export default function AddToCart() {
+  const { products, totalPrice, totalQuantity } = useSelector((state: any) => state.CartReducer);
+  const dispatch = useDispatch();
   return (
-    <div>AddToCart</div>
+    <>
+      <div className="container mt-5">
+
+        <div className="row pt-5">
+          <h1>Your cart</h1>
+          {
+            products.length > 0 ?
+              <>
+                <div className="col-12 col-lg-8">
+                  <table className="table table-striped">
+                    <tr>
+                      <th>Image</th>
+                      <th>Name</th>
+                      <th>Price</th>
+                      <th>Inc/dec</th>
+                      <th>Total Price</th>
+                      <th>Remove</th>
+                    </tr>
+                    <tbody>
+                      {
+                        products.map((singleItem: any) =>
+                          <>
+                            <tr key={singleItem.id}>
+                              <td><img src={`/images/${singleItem.image}`} style={{width:"100px",height:"40px",objectFit:"contain"}} alt="" /></td>
+                              <td>{singleItem.name}</td>
+                              <td>{singleItem.discountPrice}</td>
+                              <td>
+                                <div className="incDec d-flex justify-contents-between">
+                                  <span className='ps-1 pe-1 border rounded' onClick={() => {dispatch({type:"DEC", id: singleItem.id})}}><BsDash /></span>
+                                  <span className='ps-1 pe-1 border rounded'>{singleItem.quantity}</span>
+                                  <span className='ps-1 pe-1 border rounded' onClick={() => {dispatch({type:"INC",id:singleItem.id})}}><BsPlus /></span>
+                                </div>
+                              </td>
+                              <td>{singleItem.discountPrice * singleItem.quantity}</td>
+                              <td onClick={() => {dispatch({type:"REMOVE", id:singleItem.id})}}><BsXCircleFill /></td>
+                            </tr>
+                          </>
+                        )
+                      }
+                    </tbody>
+                  </table>
+                </div>
+                <div className="col-12 col-lg-4">
+                  <h4>Your cart summery</h4>
+                  <div className="totalProduct">
+                    <h6>Total Product: {totalQuantity}</h6>
+                  </div>
+                  <div className="totalPrice">
+                    <h6>Total Price: {totalPrice}</h6>
+                  </div>
+                  <button>Checkout</button>
+                </div>
+              </>
+
+              : "you dont Have any products"
+          }
+        </div>
+      </div>
+    </>
   )
 }
--- src/store/reducers/CartReducer.tsx
+++ src/store/reducers/CartReducer.tsx
@@ -1,23 +1,55 @@
 const initialState = {
-  products: [{id:0}],
+  products: [],
   totalPrice: 0,
   totalQuantity: 0
 }
 
-export default function CartReduces(state = initialState, action: any) {
-
+export default function CartReducer(state:any = initialState, action: any) {
+  let findPro: any;
+  let filterPro: any;
+  let index: any;
   switch (action.type) {
     case "AddTOCart":
       const { product, quantity } = action.payload;
-      const checkProduct = state.products.find(pr => pr.id === product.id);
+      console.log(product)
+      const checkProduct = state.products && state.products.find((pr:any) => pr.id === product.id);
       if (checkProduct) {
         return state;
       } else {
         const tPrice = state.totalPrice + (product.discountPrice * quantity);
-        const tQuantity = state.totalQuantity + quantity
-        return { ...state, products: [...state.products, product], totalPrice: tPrice, totalQuantity: tQuantity }
+        const tQuantity = state.totalQuantity + quantity;
+        const tProduct = [...state.products, product]
+        product.quantity  = quantity;
+        return { ...state, products: tProduct, totalPrice: tPrice, totalQuantity: tQuantity }
       }
-
+    case "INC": {
+      findPro = state.products.find((pr:any) => pr.id === action.id);
+      index = state.products.findIndex((pr:any) => pr.id === action.id);
+      findPro.quantity += 1;
+      state.products[index] = findPro;
+      return {
+        ...state, totalPrice: state.totalPrice + findPro.discountPrice, totalQuantity: state.totalQuantity + 1
+      }
+    }
+    case "DEC":
+      findPro = state.products.find((pr:any) => pr.id === action.id);
+      index = state.products.find((pr:any) => pr.id === action.id);
+      findPro.quantity -= 1;
+      state.products[index] = findPro;
+      return {
+        ...state,
+        totalPrice: state.totalPrice - findPro.discountPrice,
+        totalQuantity: state.totalQuantity - 1
+      }
+    case "REMOVE":
+      findPro = state.products.find((pr:any) => pr.id === action.id)
+      filterPro = state.products.filter((pr:any) => pr.id !== action.id)
+      return {
+          ...state,
+          products: filterPro,
+          totalPrice: state.totalPrice - (findPro.discountPrice * findPro.quantity),
+          totalQuantity: state.totalQuantity - findPro.quantity,
+      }
     default:
       return state;
   }
